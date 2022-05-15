6d3b2e8e777d72a3474cd0e77c4a0c6fe37d23e4
AshotHeb <58462105+AshotHeb@users.noreply.github.com>
2022-05-15 09:55:44 +0400

    Selectors

---
+++ js/selectors.js
@@ -0,0 +1,52 @@
+//1.getElementsByClassName
+// const boxElements = document.getElementsByClassName('box')
+// console.log("🚀 ~ boxElement", boxElements)
+
+// const listItemsElements = document.getElementsByClassName('list-item')
+// console.log("🚀 ~ listItemsElements", listItemsElements)
+// console.log('listItemsElements[0]' ,listItemsElements[0]);
+
+//HTMLCollection =>only HTML elements  
+
+//2.getElementById
+
+// const registerForm = document.getElementById('regiterForm')
+// console.log("🚀 ~ registerForm", registerForm)
+
+
+//3. getElementByTagName
+
+// const allPTags = document.getElementsByTagName('p')
+// console.log("🚀 ~ allPTags", allPTags)
+
+//4. getElementsByName
+
+// const collection = document.getElementsByName('name')
+// console.log("🚀 ~ collection", collection)
+
+
+//5.querySelector 
+// const list = document.querySelector('.list')
+// const form = document.querySelector('#regiterForm')
+// console.log("🚀 ~ list", list)
+// console.log("🚀 ~ form", form)
+
+
+//6.querySelectorAll
+
+
+// const listItems = document.querySelectorAll('.list-item')
+// let firstSize  =20
+// listItems.forEach(item=>{
+//     const randomNumber = Math.floor(Math.random() * 255)
+//     item.style.color = `rgb(${randomNumber}, 34,56)`
+//     item.style.fontSize = `${firstSize}px` 
+//     firstSize+=5
+// })
+
+// const box = document.querySelector('.box')
+// box.style.width = '100px'
+// box.style.height = '100px'
+// box.style.backgroundColor = 'red' //background-color border-radius => borderRadius
+
+
